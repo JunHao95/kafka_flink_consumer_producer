f07b6fa06041a31d9d54a80223de8247439ffca8
Jun Hao, Garren <teejunhao@gmail.com>
2024-03-21 22:19:44 +0800

    My first commit

---
+++ src/UserConsumer.java
@@ -0,0 +1,44 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+//import org.apache.kafka.common.serialization.DoubleDeserializer;
+//import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOptions;
+
+import java.util.*;
+
+public class UserConsumer {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the name for the consumer:");
+        String consumerName = scanner.nextLine().trim();
+
+        KafkaReceiver<String, String> receiver = createReceiver(List.of("user-alerts"), "balance-consumer");
+        receiver.receive().subscribe(event -> {
+          if (event.key().equals(consumerName)) {
+            System.out.printf("%s: %s\n", event.key(), event.value());
+            event.receiverOffset().commit().subscribe();
+          }
+        });
+//        KafkaReceiver<String, String> txnAlerts = createReceiver(List.of("user-alerts"), "alert-consumer");
+//        txnAlerts.receive().doOnNext(event -> {
+//            long timestamp = event.timestamp();
+//            Date date = new Date(timestamp);
+//            System.out.printf("ALERT: potential fraudulent transaction at %s: user %s amount %s\n", date, event.key(), event.value());
+//            event.receiverOffset().commit().subscribe();
+//        }).blockLast();
+    }
+
+    public static KafkaReceiver<String, String> createReceiver(Collection<String> topics, String groupId) {
+        Properties p = new Properties();
+        p.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
+        p.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        p.put(ConsumerConfig.CLIENT_ID_CONFIG, "consumer-users");
+        p.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        p.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        p.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        p.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        ReceiverOptions<String, String> receiverOptions = ReceiverOptions.create(p);
+        return KafkaReceiver.create(receiverOptions.subscription(topics));
+    }
+}
---
+++ src/UserProducer.java
@@ -0,0 +1,47 @@
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.kafka.sender.KafkaSender;
+import reactor.kafka.sender.SenderRecord;
+import utils.KafkaConfiguration;
+import java.util.Scanner;
+
+public class UserProducer {
+
+    private static final String TOPIC_BALANCE = "user-balances";
+
+
+    public static void main(String[] args) {
+
+        KafkaSender<String, String> balanceSender = KafkaConfiguration.createSender(StringSerializer.class, StringSerializer.class, "Name");
+
+        try (Scanner scanner = new Scanner(System.in)) {
+          System.out.println("Enter your name:");
+          String senderName = scanner.nextLine().trim(); // sender name
+
+          System.out.println("Enter recipient name:");
+          String targetUserName = scanner.nextLine().trim(); // target name?
+
+            // Prompt for messages to send
+            System.out.println("Enter messages (type 'exit' to quit):");
+            // Create a Flux from user input
+            Flux.<ProducerRecord<String, String>>create(sink -> {
+                        while (scanner.hasNextLine()) {
+                            String input = scanner.nextLine();
+                            if ("exit".equalsIgnoreCase(input)) {
+                                sink.complete(); // Complete the Flux stream
+                                return;
+                            } else {
+                                sink.next(new ProducerRecord<>(TOPIC_BALANCE, targetUserName, input));
+                            }
+                        }
+                    })
+                    .doOnNext(x -> System.out.printf("%s -> %s\n", x.key(), x.value()))
+                    .map(x -> Mono.just(SenderRecord.create(x, System.currentTimeMillis())))
+                    .flatMap(balanceSender::send)
+                    .then()
+                    .block();
+        }
+    }
+}
